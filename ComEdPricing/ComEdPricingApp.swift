2edb83d22f172686e8b76cf6c3f7ff6f78a2948a
Bhuvi Singh <bhuvisingh15@gmail.com>
2026-01-25 12:10:23 -0600

    Initial Commit

---
+++ ComEdPricing/ComEdPricingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComEdPricingApp.swift
+//  ComEdPricing
+//
+//  Created by Bhuvi Singh on 1/25/26.
+//
+
+import SwiftUI
+
+@main
+struct ComEdPricingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ComEdPricingTests/ComEdPricingTests.swift
@@ -0,0 +1,17 @@
+//
+//  ComEdPricingTests.swift
+//  ComEdPricingTests
+//
+//  Created by Bhuvi Singh on 1/25/26.
+//
+
+import Testing
+@testable import ComEdPricing
+
+struct ComEdPricingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
